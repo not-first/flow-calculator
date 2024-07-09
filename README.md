5e3015325b23fd6121167bcb07c42e362654309f
fakerr <111339712+not-first@users.noreply.github.com>
2024-07-09 20:23:09 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# flow-calculator
+
+A simple visual node-based calculator.
+Made in typescript + react, using react-flow and shadcn-ui.
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">Hello World</div>
+  );
+}
+
+export default App;
